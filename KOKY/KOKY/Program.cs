92bfffd7d813b7749a1b53f0d5b48c40f924a7c5
fooldreen <28122007arsenii@gmail.com>
2021-11-16 19:14:28 +0300

    И ещё

--- KOKY/KOKY/Program.cs
+++ KOKY/KOKY/Program.cs
@@ -6,7 +6,7 @@ namespace KOKY
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worlde!");
+            Console.WriteLine("Элиза!");
         }
     }
 }
