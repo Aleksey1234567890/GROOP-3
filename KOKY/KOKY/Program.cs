0990a8f8f80198dd19e1b0ab8824830260afa2c7
Tenter24 <amirsb2009@gmail.com>
2021-11-16 19:26:23 +0300

    Шутка

--- KOKY/KOKY/Program.cs
+++ KOKY/KOKY/Program.cs
@@ -6,7 +6,7 @@ namespace KOKY
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Элиза!");
+            Console.WriteLine("Элизка - сосиска!");
         }
     }
 }
